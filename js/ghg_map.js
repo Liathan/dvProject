58d6c73572131d426c79adaa49b45936e2640012
liathan <liathancorvinus@gmail.com>
2024-09-16 13:56:54 +0200

    Animato la mappa mandando avanti lgi anni

--- js/ghg_map.js
+++ js/ghg_map.js
@@ -17,7 +17,6 @@ const tonnesPC = new Map()
 var maxPC = 0
 var minPC = 1e6
 
-const quarters = Array("2010-Q1" ,"2010-Q2" ,"2010-Q3" ,"2010-Q4" ,"2011-Q1" ,"2011-Q2" ,"2011-Q3" ,"2011-Q4" ,"2012-Q1" ,"2012-Q2" ,"2012-Q3" ,"2012-Q4" ,"2013-Q1" ,"2013-Q2" ,"2013-Q3" ,"2013-Q4" ,"2014-Q1" ,"2014-Q2" ,"2014-Q3" ,"2014-Q4" ,"2015-Q1" ,"2015-Q2" ,"2015-Q3" ,"2015-Q4" ,"2016-Q1" ,"2016-Q2" ,"2016-Q3" ,"2016-Q4" ,"2017-Q1" ,"2017-Q2" ,"2017-Q3" ,"2017-Q4" ,"2018-Q1" ,"2018-Q2" ,"2018-Q3" ,"2018-Q4" ,"2019-Q1" ,"2019-Q2" ,"2019-Q3" ,"2019-Q4" ,"2020-Q1" ,"2020-Q2" ,"2020-Q3" ,"2020-Q4" ,"2021-Q1" ,"2021-Q2" ,"2021-Q3" ,"2021-Q4" ,"2022-Q1" ,"2022-Q2" ,"2022-Q3" ,"2022-Q4" ,"2023-Q1" ,"2023-Q2" ,"2023-Q3" ,"2023-Q4" ,"2024-Q1" )
 
 var colorScaleAbs
 var colorScalePC 
@@ -30,6 +29,52 @@ const svg_map = d3.select(id_map)
         ' ' + (height_map + margin_map.top + margin_map.bottom))
     .append("g")
 var pippo
+
+function redrawMap(idx)
+{
+    console.log(+idx)
+    nameArr.forEach( (el,i) => {
+        svg_map.select("."+el)
+        .attr("fill", colorScalePC(tonnesPC.get(quarters[+idx])[i]))
+    });
+    svg_map.selectAll(".ND").attr("fill", "#737373")
+}
+var isPlaying = false
+var intervalID
+var playButton = document.getElementById("playButton")
+var select = document.getElementById("selectQuarter")
+
+function tick()
+{
+    var value = +select.value +1
+    if (value == quarters.length -1)
+    {
+        isPlaying = !isPlaying
+        clearInterval(intervalID)
+        playButton.innerHTML = "<i class='fa fa-play'></i>"
+    }
+    select.value = +value
+    select.dispatchEvent(new Event("change"))
+}
+
+function slider()
+{
+    if(isPlaying)
+    {
+        clearInterval(intervalID)
+        playButton.innerHTML = "<i class='fa fa-play'></i>"
+    }
+    else
+    {
+        var idx = +select.value +1
+        if(idx == quarters.length)
+            select.value = 0
+        intervalID = setInterval(tick, 300)
+        select.value = String(idx)
+    }
+    isPlaying = !isPlaying
+}
+
 Promise.all([
     d3.json("../data/europe_.geojson"),
     d3.csv("../data/perCapita.csv").then( function (d) {
@@ -76,10 +121,10 @@ Promise.all([
             .style("fill-opacity", "0.9")
             .style("stroke", "white")
             .style("stroke-width", "1px");
-        
-        nameArr.forEach( (el,i) => {
-            svg_map.select("."+el)
-            .attr("fill", colorScalePC(tonnesPC.get("2010-Q1")[i]))
-        });
-        svg_map.selectAll(".ND").attr("fill", "#737373")
+        redrawMap(0)
+        d3.select("#selectQuarter").on("change", function (d) {
+            console.log(this)
+            redrawMap(this.value)
+        })
+        playButton.onclick = slider
     })
