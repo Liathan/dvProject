30d76cfd02bfc4b01ac4136ef1fec7eb4fec7dc3
liathan <liathancorvinus@gmail.com>
2024-09-30 11:36:18 +0200

    Aggiunto la palette e ora i colori funzionano

--- js/byActivty.js
+++ js/byActivty.js
@@ -13,9 +13,9 @@ svg_stacked = d3.select(id_stacked_percentage)
         ' ' + (height_stacked + margin_stacked.top + margin_stacked.bottom))
     .append("g")
 
-y = d3.scaleLinear().domain([0, 26000]).range([0, height_stacked])
+y = d3.scaleLinear().domain([0, 1]).range([0, height_stacked])
 var pippo
-d3.csv("data/THS_T_nace.csv").then(function (data) {
+d3.csv("data/percentageTHS.csv").then(function (data) {
     pippo = data
     x = d3.scaleLinear().domain(d3.extent(data, d => d.year)).range([0, width_stacked])
     for (const nation of nameMap.keys()) {
@@ -30,7 +30,7 @@ d3.csv("data/THS_T_nace.csv").then(function (data) {
         .y1(function(d) { return y(d[1]); })
 
     color = d3.scaleOrdinal().domain(d3.union(coso.map(d => d.key[0]))).range(d3.schemeSpectral[11])
-    svg_stacked.selectAll("path").data(coso).join("path").attr("d", area).style("fill", "red")
+    svg_stacked.selectAll("path").data(coso).join("path").attr("d", area).style("fill", d => palette.get(d.key))
 
 })
 
